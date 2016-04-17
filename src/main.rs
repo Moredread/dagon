ec024c23f76374be9957d8a6e226b90dcabf27e7
André-Patrick Bubel <code@andre-bubel.de>
2016-04-17 17:30:49 +0000

    Move from newt-onion law of gravity to newton's XD

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::ops::Add;
-
 struct Data {
     density: f64,
     velocity: [f64; 2],
@@ -21,8 +19,20 @@ struct GravityParticle {
     mass: f64,
 }
 
-fn newt_onion_gravity(m1: f64, m2: f64, r: f64) -> f64 {
-    - m1 * m2 / r.powi(2)
+fn newtonian_gravity(m1: f64, m2: f64, p1: [f64; 2], p2: [f64;2]) -> [f64; 2] {
+    scal_mul(- m1 * m2 / norm(diff(p2, p1)).powi(2), diff(p2, p1))
+}
+
+fn scal_mul(a:f64, v: [f64; 2]) -> [f64; 2] {
+    [a * v[0], a * v[1]]
+}
+
+fn add(p1: [f64; 2], p2: [f64; 2]) -> [f64; 2] {
+    [p1[0] + p2[0], p1[1] + p2[1]]
+}
+
+fn diff(p1: [f64; 2], p2: [f64; 2]) -> [f64; 2] {
+    [p1[0] - p2[0], p1[1] - p2[1]]
 }
 
 fn norm(v: [f64; 2]) -> f64 {
@@ -33,15 +43,14 @@ fn distance(pos1: [f64;2], pos2: [f64; 2]) -> f64 {
     norm([pos2[0] - pos1[0], pos2[1] - pos1[1]])
 }
 
-fn force_between_particles(particle_1: GravityParticle, particle_2: GravityParticle) -> f64 {
-    newt_onion_gravity(particle_1.mass, particle_2.mass, distance(particle_1.position, particle_2.position))
+fn force_between_particles(particle_1: GravityParticle, particle_2: GravityParticle) -> [f64; 2] {
+    newtonian_gravity(particle_1.mass, particle_2.mass, particle_1.position, particle_2.position)
 }
 
-fn sum_force<I: Iterator<Item = GravityParticle>>(particle: GravityParticle, all_particles: I) -> f64 {
+fn sum_force<I: Iterator<Item = GravityParticle>>(particle: GravityParticle, all_particles: I) -> [f64; 2] {
     all_particles.filter(|&x| x != particle).
-        map(|x| force_between_particles(x, particle)).fold(0f64, Add::add)
+        map(|x| force_between_particles(x, particle)).fold([0f64, 0f64], add)
 }
 
 fn main() {
-    
 }
