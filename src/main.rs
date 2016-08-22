a437d163a6e125baad443ca83e901423286780c7
André-Patrick Bubel <code@andre-bubel.de>
2016-08-22 14:51:43 +0200

    Moved to nalgebra types

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ extern crate rand;
 extern crate nalgebra as na;
 
 use rand::distributions::{IndependentSample, Range};
-use na::{Vector2};
+use std::ops::Add;
+use na::{Vector2, Norm};
 
 type Vector = Vector2<f64>;
 
@@ -25,23 +26,7 @@ struct GravityParticle {
 }
 
 fn newtonian_gravity(m1: f64, m2: f64, p1: Vector, p2: Vector) -> Vector {
-    scal_mul(- m1 * m2 / norm(diff(p2, p1)).powi(2), diff(p2, p1))
-}
-
-fn scal_mul(a:f64, v: Vector) -> Vector {
-    Vector::new(a * v[0], a * v[1])
-}
-
-fn add(p1: Vector, p2: Vector) -> Vector {
-    Vector::new(p1[0] + p2[0], p1[1] + p2[1])
-}
-
-fn diff(p1: Vector, p2: Vector) -> Vector {
-    Vector::new(p1[0] - p2[0], p1[1] - p2[1])
-}
-
-fn norm(v: Vector) -> f64 {
-    (v[0] * v[0] + v[1] * v[1]).sqrt()
+    (- m1 * m2 / (p2 - p1).norm_squared()) * (p2 - p1).normalize()
 }
 
 fn force_between_particles(particle_1: GravityParticle, particle_2: GravityParticle) -> Vector {
@@ -50,7 +35,7 @@ fn force_between_particles(particle_1: GravityParticle, particle_2: GravityParti
 
 fn sum_force<'a, I: Iterator<Item = &'a GravityParticle>>(particle: GravityParticle, all_particles: I) -> Vector {
     all_particles.filter(|x| **x != particle).
-        map(|x| force_between_particles(*x, particle)).fold(Vector::new(0f64, 0f64), add)
+        map(|x| force_between_particles(*x, particle)).fold(Vector::new(0f64, 0f64), Vector::add)
 }
 
 fn main() {
@@ -76,8 +61,8 @@ fn main() {
         }
 
         for i in 0..ps.len() {
-            ps[i].velocity = add(ps[i].velocity, scal_mul(timestep, forces[i]));
-            ps[i].position = add(ps[i].position, scal_mul(timestep, ps[i].velocity));
+            ps[i].velocity = ps[i].velocity + timestep * forces[i];
+            ps[i].position = ps[i].position + timestep * ps[i].velocity;
         }
 
         t += timestep;
